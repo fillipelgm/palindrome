a5ab4370b24b8078db79de8ca65a8af24685a72b
Fillipe Madureira <39290405+fillipelgm@users.noreply.github.com>
2020-12-30 22:29:26 -0300

    Included more examples

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from palindrome import isPalindrome
 
-strings = ['arara', 'Hannah', 'only', 'radar', 'level', 'amora', "A man, a plan, a canal – Panama"]
+strings = ['arara',
+           'Hannah', 
+           'only', 
+           'radar', 
+           'level', 
+           'amora', 
+           "A man, a plan, a canal – Panama", 
+           "A Toyota! Race fast, safe car! A Toyota!",
+           "Are we not drawn onward to new era?",
+           "Are Mac ‘n’ Oliver ever evil on camera?"]
 
 for string in strings:
     print(string, isPalindrome(strings))
